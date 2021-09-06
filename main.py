7f05152f42721ecdaf56ec086389c0afe1baf961
jarvis08 <jarvis08.cdb@gmail.com>
2021-09-06 21:36:15 +0900

    modified default clustering model from BIRCH to K-means

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ parser.add_argument('--bit', default=32, type=int, help='Target bit-width to be 
 parser.add_argument('--smooth', default=0.999, type=float, help='Smoothing parameter of EMA')
 parser.add_argument('--folded_fq', default=False, type=bool, help="Fake Quantize CONV's weight after folding BatchNormalization")
 
-parser.add_argument('--clustering_method', default='birch', type=str, help="Clustering method(BIRCH/K-means) to use in PCQ")
+parser.add_argument('--clustering_method', default='kmeans', type=str, help="Clustering method(K-means/BIRCH) to use in PCQ")
 parser.add_argument('--cluster', default=1, type=int, help='Number of clusters')
 parser.add_argument('--partition', default=4, type=int, help="Number of partitions to divide a channel in kmeans clustering's input")
 parser.add_argument('--clustering_path', default='', type=str, help="Trained K-means clustering model's path")
