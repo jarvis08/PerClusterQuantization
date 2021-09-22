d0e3b7df272c4de3f4e6ac2ad8ebaad4c8e87621
jarvis08 <jarvis08.cdb@gmail.com>
2021-09-22 17:07:57 +0900

    move custom dali modules to backup

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,3 @@
 from .misc import *
 from .torch_dataset import *
-# from .dali_loader import *
-# from .dali import *
-from .dali_pytorch import *
-from .dali_official_loader import *
+from .dali import *
