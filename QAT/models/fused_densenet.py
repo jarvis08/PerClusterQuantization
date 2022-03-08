af302f5e942fad1d44bc8477459658d8fcbd5666
Yongjoo Lee <yongjuice@gmail.com>
2022-03-08 15:57:16 +0900

    densenet bit config

--- QAT/models/fused_densenet.py
+++ QAT/models/fused_densenet.py
@@ -21,19 +21,18 @@ class FusedDenseLayer(nn.Module):
         memory_efficient: bool = False
     ) -> None:
         super(FusedDenseLayer, self).__init__()
-        self.arg_dict = arg_dict
 
-        self.bit, self.smooth, self.runtime_helper, self.use_ste, self.quant_noise, self.qn_prob \
-            = itemgetter('bit', 'smooth', 'runtime_helper', 'ste', 'quant_noise', 'qn_prob')(arg_dict)
-        self.q_max = 2 ** self.bit - 1
-        self.act_qmax = 2 ** 16 - 1
+        target_bit, bit_conv_act, bit_addcat, self.smooth, self.use_ste, self.num_clusters, self.runtime_helper \
+            = itemgetter('bit', 'bit_conv_act', 'bit_addcat', 'smooth', 'ste', 'cluster', 'runtime_helper')(arg_dict)
+        self.a_bit = torch.nn.Parameter(torch.tensor(bit_conv_act, dtype=torch.int8), requires_grad=False)
+        self.target_bit = torch.nn.Parameter(torch.tensor(target_bit, dtype=torch.int8), requires_grad=False)
 
-        self.bn1 = FusedBnReLU(num_input_features, nn.ReLU, arg_dict=arg_dict)
+        self.bn1 = FusedBnReLU(num_input_features, activation=nn.ReLU, a_bit=target_bit, arg_dict=arg_dict)
         self.conv1 = FusedConv2d(num_input_features, bn_size * growth_rate, kernel_size=1, stride=1, bias=False,
-                                 arg_dict=arg_dict, act_qmax=self.act_qmax)
-        self.bn2 = FusedBnReLU(bn_size * growth_rate, nn.ReLU, arg_dict=arg_dict)
+                                 arg_dict=arg_dict, a_bit=bit_conv_act)
+        self.bn2 = FusedBnReLU(bn_size * growth_rate, activation=nn.ReLU, a_bit=target_bit, arg_dict=arg_dict)
         self.conv2 = FusedConv2d(bn_size * growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False,
-                                 arg_dict=arg_dict, act_qmax=self.act_qmax)
+                                 arg_dict=arg_dict, a_bit=bit_conv_act)
         self.memory_efficient = memory_efficient
 
     # torchscript does not yet support *args, so we overload method
@@ -62,15 +61,15 @@ class FusedDenseLayer(nn.Module):
 class FusedTransition(nn.Sequential):
     def __init__(self, arg_dict, num_input_features: int, num_output_features: int) -> None:
         super(FusedTransition, self).__init__()
-        self.arg_dict = arg_dict
-        self.bit, self.smooth, self.runtime_helper, self.use_ste, self.quant_noise, self.qn_prob \
-            = itemgetter('bit', 'smooth', 'runtime_helper', 'ste', 'quant_noise', 'qn_prob')(arg_dict)
-        self.q_max = 2 ** self.bit - 1
-        self.act_qmax = 2 ** 16 - 1
 
-        self.bn = FusedBnReLU(num_input_features, nn.ReLU, arg_dict=arg_dict)
+        target_bit, bit_conv_act, bit_addcat, self.smooth, self.use_ste, self.num_clusters, self.runtime_helper \
+            = itemgetter('bit', 'bit_conv_act', 'bit_addcat', 'smooth', 'ste', 'cluster', 'runtime_helper')(arg_dict)
+        self.a_bit = torch.nn.Parameter(torch.tensor(bit_conv_act, dtype=torch.int8), requires_grad=False)
+        self.target_bit = torch.nn.Parameter(torch.tensor(target_bit, dtype=torch.int8), requires_grad=False)
+
+        self.bn = FusedBnReLU(num_input_features, activation=nn.ReLU, a_bit=target_bit, arg_dict=arg_dict)
         self.conv = FusedConv2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False,
-                                arg_dict=arg_dict, act_qmax=self.act_qmax)
+                                arg_dict=arg_dict, a_bit=bit_conv_act)
         self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
 
     def forward(self, x, next_block_range):
@@ -100,14 +99,12 @@ class FusedDenseBlock(nn.ModuleDict):
         super(FusedDenseBlock, self).__init__()
         self.arg_dict = arg_dict
         self.num_layers = num_layers
-        self.bit, self.smooth, self.runtime_helper, self.use_ste, self.quant_noise, self.qn_prob \
-            = itemgetter('bit', 'smooth', 'runtime_helper', 'ste', 'quant_noise', 'qn_prob')(arg_dict)
-        self.q_max = 2 ** self.bit - 1
-        self.act_qmax = 2 ** 16 - 1
+        target_bit, bit_conv_act, bit_addcat, self.smooth, self.use_ste, self.num_clusters, self.runtime_helper \
+            = itemgetter('bit', 'bit_conv_act', 'bit_addcat', 'smooth', 'ste', 'cluster', 'runtime_helper')(arg_dict)
+        self.a_bit = torch.nn.Parameter(torch.tensor(bit_conv_act, dtype=torch.int8), requires_grad=False)
 
         self.act_range = nn.Parameter(torch.zeros(2), requires_grad=False)
-
-        self.apply_ema = False
+        self.apply_ema = nn.Parameter(torch.tensor(0, dtype=torch.bool), requires_grad=False)
 
         for i in range(num_layers):
             layer = FusedDenseLayer(
@@ -129,16 +126,15 @@ class FusedDenseBlock(nn.ModuleDict):
         if not self.training:
             return out
 
-        _out = out
         if self.apply_ema:
             self.act_range[0], self.act_range[1] = ema(out, self.act_range, self.smooth)
         else:
             self.act_range[0], self.act_range[1] = get_range(out)
             self.apply_ema.data = torch.tensor(True, dtype=torch.bool)
-        return _out
+        return out
 
     def set_block_qparams(self):
-        self.s3, self.z3 = calc_qparams(self.act_range[0], self.act_range[1], self.act_qmax)
+        self.s3, self.z3 = calc_qparams(self.act_range[0], self.act_range[1], self.a_bit)
         for name, layer in self.items():
             layer.set_layer_qparams(self.s3, self.z3)
         return self.s3, self.z3
@@ -157,19 +153,21 @@ class FusedDenseNet(nn.Module):
     ) -> None:
         super(FusedDenseNet, self).__init__()
         self.arg_dict = arg_dict
-        self.bit, self.smooth, self.runtime_helper, self.quant_noise, self.qn_prob \
-            = itemgetter('bit', 'smooth', 'runtime_helper', 'quant_noise', 'qn_prob')(arg_dict)
-        self.q_max = 2 ** self.bit - 1
-        self.act_qmax = 2 ** 16 - 1
-        self.in_range = nn.Parameter(torch.zeros(2), requires_grad=False)
+        target_bit, self.bit_conv_act, bit_addcat, bit_first, bit_classifier, self.smooth, self.num_clusters, self.runtime_helper \
+            = itemgetter('bit', 'bit_conv_act', 'bit_addcat', 'bit_first', 'bit_classifier', 'smooth', 'cluster',
+                         'runtime_helper')(arg_dict)
+        self.target_bit = torch.nn.Parameter(torch.tensor(target_bit, dtype=torch.int8), requires_grad=False)
+        self.a_bit = torch.nn.Parameter(torch.tensor(bit_addcat, dtype=torch.int8), requires_grad=False)
+        self.in_bit = torch.nn.Parameter(torch.tensor(bit_first, dtype=torch.int8), requires_grad=False)
 
-        self.apply_ema = False
+        self.in_range = nn.Parameter(torch.zeros(2), requires_grad=False)
+        self.apply_ema = nn.Parameter(torch.tensor(0, dtype=torch.bool), requires_grad=False)
 
         # First convolution
         self.features = nn.Sequential(OrderedDict([
             ('first_conv', FusedConv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False,
-                                       arg_dict=arg_dict, act_qmax=self.act_qmax)),
-            ('first_norm', FusedBnReLU(num_init_features, nn.ReLU, act_qmax=self.act_qmax, arg_dict=arg_dict)),
+                                       w_bit=bit_first, a_bit=self.bit_conv_act, arg_dict=arg_dict)),
+            ('first_norm', FusedBnReLU(num_init_features, activation=nn.ReLU, a_bit=bit_addcat, arg_dict=arg_dict)),
             ('maxpool', nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
         ]))
 
@@ -191,17 +189,17 @@ class FusedDenseNet(nn.Module):
                 self.features.add_module('transition%d' % (i + 1), trans)
                 num_features = num_features // 2
         # Last Norm
-        self.features.add_module('last_norm', FusedBnReLU(num_features, nn.ReLU, arg_dict=arg_dict))
+        self.features.add_module('last_norm', FusedBnReLU(num_features, activation=nn.ReLU, a_bit=target_bit, arg_dict=arg_dict))
         # Linear layer
-        self.classifier = FusedLinear(num_features, num_classes, arg_dict=arg_dict)
+        self.classifier = FusedLinear(num_features, num_classes, is_classifier=True, w_bit=bit_classifier, a_bit=bit_classifier, arg_dict=arg_dict)
 
     def forward(self, x: Tensor) -> Tensor:
         if self.training:
             if self.apply_ema:
                 self.in_range[0], self.in_range[1] = ema(x, self.in_range, self.smooth)
                 if self.runtime_helper.apply_fake_quantization:
-                    s, z = calc_qparams(self.in_range[0], self.in_range[1], self.q_max)
-                    x = fake_quantize(x, s, z, self.q_max)
+                    s, z = calc_qparams(self.in_range[0], self.in_range[1], self.in_bit)
+                    x = fake_quantize(x, s, z, self.in_bit)
             else:
                 self.in_range[0], self.in_range[1] = get_range(x)
                 self.apply_ema.data = torch.tensor(True, dtype=torch.bool)
@@ -225,7 +223,7 @@ class FusedDenseNet(nn.Module):
         return out
 
     def set_quantization_params(self):
-        self.scale, self.zero_point = calc_qparams(self.in_range[0], self.in_range[1], self.q_max)
+        self.scale, self.zero_point = calc_qparams(self.in_range[0], self.in_range[1], self.in_bit)
         conv_s, conv_z = self.features.first_conv.set_qparams(self.scale, self.zero_point)
         block1_s, block1_z = self.features.denseblock1.set_block_qparams()
         block2_s, block2_z = self.features.denseblock2.set_block_qparams()
@@ -244,9 +242,12 @@ def fused_densenet(arg_dict: dict, **kwargs):
 
 
 def set_fused_densenet(fused, pre):
+    bn_momentum = fused.arg_dict['bn_momentum']
     # first conv & norm
-    fused.features.first_conv = copy_from_pretrained(fused.features.first_conv, pre.features.conv0)
+    fused.features.first_conv = copy_weight_from_pretrained(fused.features.first_conv, pre.features.conv0)
     fused.features.first_norm = copy_bn_from_pretrained(fused.features.first_norm, pre.features.norm0)
+    fused.features.first_norm.bn_momentum = bn_momentum
+
     # dense block & Transition
     for block_idx in range(1,5):
         fused_block = getattr(fused.features, 'denseblock%d' % block_idx)
@@ -259,16 +260,20 @@ def set_fused_densenet(fused, pre):
             fused_layer = getattr(fused_block,'denselayer%d' % layer_idx)
             pre_layer = getattr(pre_block,'denselayer%d' % layer_idx)
             fused_layer.bn1 = copy_bn_from_pretrained(fused_layer.bn1, pre_layer.norm1)
-            fused_layer.conv1 = copy_from_pretrained(fused_layer.conv1, pre_layer.conv1)
+            fused_layer.bn1.bn_momentum = bn_momentum
+            fused_layer.conv1 = copy_weight_from_pretrained(fused_layer.conv1, pre_layer.conv1)
             fused_layer.bn2 = copy_bn_from_pretrained(fused_layer.bn2, pre_layer.norm2)
-            fused_layer.conv2 = copy_from_pretrained(fused_layer.conv2, pre_layer.conv2)
+            fused_layer.bn2.bn_momentum = bn_momentum
+            fused_layer.conv2 = copy_weight_from_pretrained(fused_layer.conv2, pre_layer.conv2)
 
         # transition
         if block_idx < 4:
             fused_trans.bn = copy_bn_from_pretrained(fused_trans.bn, pre_trans.norm)
-            fused_trans.conv = copy_from_pretrained(fused_trans.conv, pre_trans.conv)
+            fused_trans.bn.bn_momentum = bn_momentum
+            fused_trans.conv = copy_weight_from_pretrained(fused_trans.conv, pre_trans.conv)
     # Last BatchNorm
     fused.features.last_norm = copy_bn_from_pretrained(fused.features.last_norm, pre.features.norm5)
+    fused.features.last_norm.bn_momentum = bn_momentum
     # Classifier
     fused.classifier = copy_from_pretrained(fused.classifier, pre.classifier)
     return fused
