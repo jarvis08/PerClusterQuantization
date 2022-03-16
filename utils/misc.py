ff9865d64d56b45ad6846021819acf15b5b8c058
kanghansung <xovndrkd@naver.com>
2022-03-16 17:41:15 +0900

    pcq_validate leftover remove

--- utils/misc.py
+++ utils/misc.py
@@ -312,25 +312,6 @@ def pcq_epoch(model, clustering_model, train_loader, criterion, optimizer, runti
             if container.ready_cluster is None:
                 break
 
-        container.check_leftover()
-        for c in range(container.num_clusters):
-            if container.leftover_cluster_data[c]:
-                input, target, runtime_helper.qat_batch_cluster = container.leftover_batch[c][0], \
-                                                                  container.leftover_batch[c][1], c
-                input, target = input.cuda(), target.cuda()
-                runtime_helper.qat_batch_cluster = torch.tensor(runtime_helper.qat_batch_cluster, dtype=torch.int,
-                                                                device='cuda', requires_grad=False)
-
-                output = model(input)
-                del runtime_helper.qat_batch_cluster
-
-                loss = criterion(output, target)
-                prec = accuracy(output, target)[0]
-                losses.update(loss.item(), input.size(0))
-                top1.update(prec.item(), input.size(0))
-
-                t.set_postfix(loss=losses.avg, acc=top1.avg)
-        del container
 
 
 def pcq_validate(model, clustering_model, test_loader, criterion, runtime_helper, logger=None, hvd=None):
@@ -372,23 +353,23 @@ def pcq_validate(model, clustering_model, test_loader, criterion, runtime_helper
 
                 t.set_postfix(loss=losses.avg, acc=top1.avg)
 
-            container.check_leftover()
-            for c in range(container.num_clusters):
-                if container.leftover_cluster_data[c]:
-                    input, target, runtime_helper.qat_batch_cluster = container.leftover_batch[c][0], container.leftover_batch[c][1], c
-                    input, target = input.cuda(), target.cuda()
-                    runtime_helper.qat_batch_cluster = torch.tensor(runtime_helper.qat_batch_cluster, dtype=torch.int, device='cuda', requires_grad=False)
-
-                    output = model(input)
-                    del runtime_helper.qat_batch_cluster
-
-                    loss = criterion(output, target)
-                    prec = accuracy(output, target)[0]
-                    losses.update(loss.item(), input.size(0))
-                    top1.update(prec.item(), input.size(0))
-
-                    t.set_postfix(loss=losses.avg, acc=top1.avg)
-            del container
+            # container.check_leftover()
+            # for c in range(container.num_clusters):
+            #     if container.leftover_cluster_data[c]:
+            #         input, target, runtime_helper.qat_batch_cluster = container.leftover_batch[c][0], container.leftover_batch[c][1], c
+            #         input, target = input.cuda(), target.cuda()
+            #         runtime_helper.qat_batch_cluster = torch.tensor(runtime_helper.qat_batch_cluster, dtype=torch.int, device='cuda', requires_grad=False)
+            #
+            #         output = model(input)
+            #         del runtime_helper.qat_batch_cluster
+            #
+            #         loss = criterion(output, target)
+            #         prec = accuracy(output, target)[0]
+            #         losses.update(loss.item(), input.size(0))
+            #         top1.update(prec.item(), input.size(0))
+            #
+            #         t.set_postfix(loss=losses.avg, acc=top1.avg)
+            # del container
 
     if logger:
         if hvd:
