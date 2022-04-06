60071c922f90c6f95294a44d2418bf3843e551e4
JaeminK <maidus90@gmail.com>
2022-04-06 09:56:11 +0000

    run_script update

--- run_HAWQ2.sh
+++ run_HAWQ2.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+export TORCH_WARN_ONCE
+
 PRETRAINED_MODEL_PATH="/workspace/pretrained_models"
-CLUSTERING_MODEL_PATH=""
+CLUSTERING_MODEL_PATH="result/kmeans/cifar100/nnac_resnet20.k4.sub8.part2.mean.topk_3.sim_0.7/"
 
 MODEL="resnet20"
 DATASET="cifar100"
@@ -16,7 +18,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --quant_base hawq \
     --arch $MODEL \
     --dataset $DATASET \
-    --lr 0.001 \
+    --lr 0.0001 \
     --act-range-momentum 0.99 \
     --wd 1e-4 \
     --fix-BN \
@@ -24,11 +26,12 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --channel-wise true \
     --quant-scheme uniform4 \
     --gpu 0 \
+    --cluster 4 \
+    --sub_cluster 8 \
+    --clustering_path $CLUSTERING_MODEL_PATH \
+    --repr_method mean \
+    --similarity_method jaccard \
     --data $DATASET \
     --batch-size $BATCH \
     --transfer_param \
     --dnn_path $PRETRAINED_MODEL_PATH/$DATASET/$PRETRAINED_MODEL/checkpoint.pth \
-    # --cluster 4 \
-    # --repr_method mean \
-    # --sub_cluster 8 \
-    # --clustering_path $CLUSTERING_MODEL_PATH \
