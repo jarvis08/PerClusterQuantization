f3ec0946c761c1a2fa6fecbfc778117b42812d3c
JaeminK <maidus90@gmail.com>
2022-04-17 18:12:55 +0000

    run script update

--- run_HAWQ.sh
+++ run_HAWQ.sh
@@ -7,22 +7,22 @@ PRETRAINED_MODEL_PATH="/workspace/pretrained_models"
 
 ###################################################
 
-GPU_NUM=0
+GPU_NUM=${1}
 
 # alexnet / resnet20 / resnet50 / densenet121
-MODEL=""
- # svhn / cifar10 / cifar100 / imagenet           
-DATASET=""
+MODEL=${2}
+# svhn / cifar10 / cifar100 / imagenet           
+DATASET=${3}
 
-CLUSTER=                # 16 / 8 / 4 / 2
-SUB_CLUSTER=            # 32 / 16 / 8 / 4
-SIM_METHOD=""           # and / jaccard
+CLUSTER=${4}                # 16 / 8 / 4 / 2
+SUB_CLUSTER=${5}            # 32 / 16 / 8 / 4
+SIM_METHOD=${6}           # and / jaccard
 REPR_METHOD="max"       # FIXED TO MAX
 
-FIRST_RUN=true          # true / false
+FIRST_RUN=${7}          # true / false
 
-BATCH=128               # 128 / 64 / 32
-LEARNING_RATE=0.001     # 0.001 / 0.0001      
+BATCH=${8}               # 128 / 64 / 32
+LEARNING_RATE=${9}     # 0.001 / 0.0001      
 
 #####################################################
 
@@ -74,7 +74,7 @@ else
                 --transfer_param \
                 --dnn_path $PRETRAINED_MODEL_PATH/$DATASET/$MODEL/checkpoint.pth
         else
-            CLUSTERING_MODEL_PATH="result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}"
+            CLUSTERING_MODEL_PATH="/workspace/PerClusterQuantization/result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}"
             CUDA_VISIBLE_DEVICES=${GPU_NUM} python main.py \
                 --mode fine \
                 --epochs 100 \
@@ -127,7 +127,7 @@ else
                 --transfer_param \
                 --dnn_path $PRETRAINED_MODEL_PATH/$DATASET/$MODEL/checkpoint.pth
         else
-            CLUSTERING_MODEL_PATH="result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}.sub${SUB_CLUSTER}.topk_3.sim_0.7.${SIM_METHOD}/"
+            CLUSTERING_MODEL_PATH="/workspace/PerClusterQuantization/result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}.sub${SUB_CLUSTER}.topk_3.sim_0.7.${SIM_METHOD}/"
             CUDA_VISIBLE_DEVICES=${GPU_NUM} python main.py \
                 --mode fine \
                 --epochs 100 \
