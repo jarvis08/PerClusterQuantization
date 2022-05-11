80797a3e1b267b3f3f01100e85f73b1d6859c3ba
JaeminK <maidus90@gmail.com>
2022-05-11 06:41:59 +0000

    update

--- Clustering/kmeans.py
+++ Clustering/kmeans.py
@@ -159,7 +159,7 @@ class KMeansClustering(object):
 
             n_prediction_cluster = self.args.sub_cluster if self.args.sub_cluster else self.args.cluster
             best_model_inertia = 9999999999999999
-            model = KMeans(n_clusters=n_prediction_cluster, random_state=0).fit(x)
+            best_model = KMeans(n_clusters=n_prediction_cluster, random_state=0).fit(x)
 
         path = self.args.clustering_path
         joblib.dump(best_model, os.path.join(path + '/checkpoint.pkl'))
