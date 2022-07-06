40d07768b788f5d546652769b82730fc31b2c348
JaeminK <maidus90@gmail.com>
2022-07-06 03:56:25 +0000

    update

--- run_HAWQ.sh
+++ run_HAWQ.sh
@@ -86,10 +86,12 @@ if [ -z ${CLUSTER} ]; then
             --dnn_path $PRETRAINED_MODEL_PATH/$DATASET/$MODEL/checkpoint.pth
     fi
 else
-    if [ "$MERGED" = true ]; then
-        CLUSTERING_MODEL_PATH="/workspace/PerClusterQuantization/result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}.sub${SUB_CLUSTER}.topk_3.sim_0.7.${SIM_METHOD}/"
-    else
-        CLUSTERING_MODEL_PATH="/workspace/PerClusterQuantization/result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}/"
+    if [ "$FIRST_RUN" = true ]; then   
+        if [ "$MERGED" = true ]; then
+            CLUSTERING_MODEL_PATH="/workspace/PerClusterQuantization/result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}.sub${SUB_CLUSTER}.topk_3.sim_0.7.${SIM_METHOD}/"
+        else
+            CLUSTERING_MODEL_PATH="/workspace/PerClusterQuantization/result/kmeans/$MODEL/$DATASET/k${CLUSTER}.part2.${REPR_METHOD}/"
+        fi
     fi
 
     if [ -z ${SUB_CLUSTER} ]; then
