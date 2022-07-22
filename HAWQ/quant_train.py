21e1b587ac46d7e1be6aa01d6a3337cc9e5d876e
JaeminK <maidus90@gmail.com>
2022-07-22 02:52:58 +0000

    update

--- HAWQ/quant_train.py
+++ HAWQ/quant_train.py
@@ -770,7 +770,10 @@ class ProgressMeter(object):
 
 def adjust_learning_rate(optimizer, epoch, args):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
+    if epoch:
+        lr = args.lr * (0.1 ** (epoch // 30))
+    else:
+        lr = args.lr * 0.01
     print('lr = ', lr)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
