72e50d5e45c780be79750331614afd3b561054ed
JaeminK <maidus90@gmail.com>
2022-08-09 04:02:16 +0000

    update

--- HAWQ/quant_train.py
+++ HAWQ/quant_train.py
@@ -771,7 +771,7 @@ def adjust_learning_rate(optimizer, epoch, args):
     if epoch:
         lr = args.lr * (0.1 ** (epoch // 30))
     else:
-        lr = args.lr * 0.01
+        lr = 0
     print('lr = ', lr)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
